42dbd4c1c58bc025ad1cdbe28e9150b074609943
asahi-jp <miuraasahi1@gmail.com>
2022-12-31 00:02:05 +0900

    antdesignの導入

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,8 +1,11 @@
+import { Button } from 'antd';
+
 export default function Index() {
   return (
     <section className="bg-white h-96">
       <div>
         検索バー
+        <Button type="primary">Primary Button</Button>
       </div>
       <div>
         タグ一覧
