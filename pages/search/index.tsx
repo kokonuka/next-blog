5fc03e712f9553fb1a214826b3a485b9cafb0e11
asahi-jp <miuraasahi1@gmail.com>
2023-01-01 01:51:06 +0900

    検索ボックスを追加

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,14 +1,22 @@
-import { Box } from '@chakra-ui/react'
+import { Stack, Box, InputGroup, InputLeftElement, Input, VStack, } from '@chakra-ui/react'
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Index() {
   return (
     <Box bg="white" height="96" minH="100vh">
-      <div>
-        検索バー
-      </div>
-      <div>
+      <VStack mt="10">
+        <VStack>
+          <InputGroup >
+            <InputLeftElement pointerEvents='none'>
+              <AiOutlineSearch/>
+            </InputLeftElement>
+            <Input placeholder='Enter Keyword' borderRadius="20" />
+          </InputGroup>
+        </VStack>
+      </VStack>
+      <Box mt="10">
         タグ一覧
-      </div>
+      </Box>
     </Box>
   )
 }
