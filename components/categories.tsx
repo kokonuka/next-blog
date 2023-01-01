5c2d38d2707e6745b0125538438f49e4728b9552
asahi-jp <miuraasahi1@gmail.com>
2023-01-01 23:58:31 +0900

    widthが画面からはみでる箇所を修正

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,4 +1,4 @@
-import { Tabs, TabList, Tab, Container } from '@chakra-ui/react'
+import { Tabs, TabList, Tab, Container, Box } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 
 export default function Categories() {
@@ -7,16 +7,18 @@ export default function Categories() {
   if(router.pathname === "/search") return null
 
   return (
-    <Tabs className='categoriesWrap' bg="white" w="100vw" overflowX="scroll" position="sticky" top="0" zIndex="1">
+    <Box bg="white" position="sticky" top="0" zIndex="1">
       <Container maxW="6xl">
-      <TabList>
-        <Tab>Category1</Tab>
-        <Tab>Category2</Tab>
-        <Tab>Category3</Tab>
-        <Tab>Category4</Tab>
-        <Tab>Category5</Tab>
-      </TabList>
+        <Tabs className='categoriesWrap' w="100%" overflowX="scroll">
+          <TabList>
+            <Tab>Category1</Tab>
+            <Tab>Category2</Tab>
+            <Tab>Category3</Tab>
+            <Tab>Category4</Tab>
+            <Tab>Category5</Tab>
+          </TabList>
+        </Tabs>
       </Container>
-    </Tabs>
+    </Box>
   )
 }
