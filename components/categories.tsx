112508542f10eef55dd5ba77e291c93810b1fb90
asahi-jp <miuraasahi1@gmail.com>
2023-01-02 00:51:11 +0900

    カテゴリーヘッダーのスタイルを追加

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,23 +1,31 @@
-import { Tabs, TabList, Tab, Container, Box } from '@chakra-ui/react'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { Container, Box, Text } from '@chakra-ui/react'
 
 export default function Categories() {
   const router = useRouter()
-
   if(router.pathname === "/search") return null
 
   return (
-    <Box bg="white" position="sticky" top="0" zIndex="1">
+    <Box bg="white" position="sticky" top="0" zIndex="2">
       <Container maxW="6xl">
-        <Tabs className='categoriesWrap' w="100%" overflowX="scroll">
-          <TabList>
-            <Tab>Category1</Tab>
-            <Tab>Category2</Tab>
-            <Tab>Category3</Tab>
-            <Tab>Category4</Tab>
-            <Tab>Category5</Tab>
-          </TabList>
-        </Tabs>
+        <Box className='categoriesWrap' display="flex" gap="7" w="100%" pt="2" overflowX="scroll">
+          <Text fontWeight="bold" color="gray.600" borderBottom="2px solid" pb="3">
+            <Link href="/">Category1</Link>
+          </Text>
+          <Text fontWeight="bold" color="gray.500" pb="3">
+            <Link href="/">Category2</Link>
+          </Text>
+          <Text fontWeight="bold" color="gray.500" pb="3">
+            <Link href="/">Category3</Link>
+          </Text>
+          <Text fontWeight="bold" color="gray.500" pb="3">
+            <Link href="/">Category4</Link>
+          </Text>
+          <Text fontWeight="bold" color="gray.500" pb="3">
+            <Link href="/">Category5</Link>
+          </Text>
+        </Box>
       </Container>
     </Box>
   )
