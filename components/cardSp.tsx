0d401be61153b838a9fdb6a69a2bebc724498d78
asahi-jp <miuraasahi1@gmail.com>
2023-01-01 22:10:07 +0900

    画像の変更

--- components/cardSp.tsx
+++ components/cardSp.tsx
@@ -23,10 +23,11 @@ export default function CardSp({ post }) {
         >
           <Image
             objectFit='cover'
+            bg="white"
             width={{ base: '75px' }}
             height={{ base: '75px' }}
             borderRadius='2xl'
-            src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+            src={post.featuredImage.node.mediaItemUrl}
             alt='Caffe Latte'
           />
           <Stack>
