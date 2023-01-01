9a340eb50e86bda8c32475b8bbe63cabd8dda6f8
asahi-jp <miuraasahi1@gmail.com>
2023-01-01 22:11:07 +0900

    記事詳細ページのスタイル修正

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -10,22 +10,28 @@ const Post = (props: Props) => {
 
   return (
     <>
-      <Container maxW="6xl" pb="10">
-        {post.categories.nodes[0].name}
-        <Text fontSize="4xl" fontWeight="bold" textAlign="center">{post.title}</Text>
-        <Box display={{ base: "block", lg: "flex" }} width={{ base: "100%", lg: "4/6" }}>
-          <Box bg="white" p="5" borderRadius="10">
+      <Container maxW="6xl" py="10">
+        <Text fontSize="4xl" fontWeight="bold" textAlign="center" color="gray.700">
+          {post.title}
+        </Text>
+        <Text mt="5" fontWeight="bold" textAlign="center" color="gray.500">
+          {post.date}
+        </Text>
+        <Box display={{ base: "block", lg: "flex" }} mt="10">
+          <Box bg="white" p="5" borderRadius="10" width={{ base: "100%", lg: "60%" }}>
             <Image
               objectFit='cover'
+              width="100%"
+              maxH="96"
               borderRadius='2xl'
               src={post.featuredImage.node.mediaItemUrl}
               alt='Caffe Latte'
             />
-            <Box py="10">
+            <Box py="10" px="5">
               <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
             </Box>
           </Box>
-          <Box display={{ base: "none", lg: "block" }} width={{ base: "100%", lg: "2/6" }} pl="5">
+          <Box display={{ base: "none", lg: "block" }} width={{ base: "100%", lg: "40%" }} pl="5">
             <Box bg="white" p="5" borderRadius="10">
               サイドバー
             </Box>
