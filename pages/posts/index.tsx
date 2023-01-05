10149a385993ff3a8ec80705a6d0df8351df3222
asahi-jp <miuraasahi1@gmail.com>
2023-01-05 16:52:07 +0900

    記事一覧ページの追加

---
+++ pages/posts/index.tsx
@@ -0,0 +1,83 @@
+import { Container, Image, Text, Box } from "@chakra-ui/react"
+import CardList from "../../components/cardList"
+
+type Props = {
+  posts: Array<Post>
+}
+
+type Post = {
+  id: string
+  title: string
+  slug: string
+  featuredImage: {
+    node: {
+      mediaItemUrl: string
+    }
+  }
+  excerpt: string
+  categories: {
+    nodes: Array<{
+      name: string
+    }>
+  }
+}
+
+const Index = ({ posts }: Props) => {
+  return (
+    <>
+      <Container maxW="6xl">
+        <Text mt="10" fontSize="3xl" fontWeight="bold" textAlign="center">Posts</Text>
+        <Box mt="10">
+          <CardList posts={posts}/>
+          <Text mt="10" color="blue.500" textAlign="center">
+            <a href="/posts">もっと見る</a>
+          </Text>
+        </Box>
+      </Container>
+    </>
+  )
+}
+
+export default Index
+
+export const getStaticProps = async () => {
+  const response = await fetch(
+    process.env.GRAPHQL_ENDPOINT!,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    },
+  )
+  const data = await response.json()
+
+  return {
+    props:{
+      posts: data.data.posts.nodes
+    }
+  }
+}
+
+const query = `query getPosts {
+  posts(after: "1") {
+    nodes {
+      id
+      slug
+      date
+      title
+      featuredImage {
+        node {
+          mediaItemUrl
+        }
+      }
+      excerpt
+      categories {
+        nodes {
+          name
+        }
+      }
+    }
+  }
+}`;
