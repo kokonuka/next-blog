61038126a90a251be4d39aeb5a16c135e4e5cb56
asahi-jp <miuraasahi1@gmail.com>
2023-01-14 00:32:10 +0900

    画面遷移時のローディングを実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Router from 'next/router';
+import { ChakraProvider } from '@chakra-ui/react'
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 import Layout from '../components/layout'
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
