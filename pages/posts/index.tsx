0fe01d98c350e44d3952f8f4c040f77f66797af7
asahi-jp <miuraasahi1@gmail.com>
2023-01-14 05:01:22 +0900

    もっと見るボタンクリック時に新規取得する処理の実装

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,14 +1,20 @@
-import { Container, Link, Text, Box } from "@chakra-ui/react"
+import { useState, useEffect } from 'react' 
+import { Container, Button, Text, Box } from "@chakra-ui/react"
+
+import { getDateDiff } from "../../lib/getDateDiff"
+
 import CardList from "../../components/cardList"
 
 type Props = {
-  posts: Array<Post>
+  posts: Array<Post>,
+  pageInfo: PageInfo
 }
 
 type Post = {
   id: string
   title: string
   slug: string
+  date: string
   featuredImage: {
     node: {
       mediaItemUrl: string
@@ -22,48 +28,16 @@ type Post = {
   }
 }
 
-const Index = ({ posts }: Props) => {
-  return (
-    <>
-      <Container maxW="6xl">
-        <Box as="section">
-        <Text mt="10" fontSize="3xl" fontWeight="bold" textAlign="center">Posts</Text>
-        <Box mt="10">
-          <CardList posts={posts}/>
-          <Text mt="10" color="blue.500" textAlign="center">
-            <Link href="/">もっと見る</Link>
-          </Text>
-        </Box>
-        </Box>
-      </Container>
-    </>
-  )
+type PageInfo = {
+  endCursor: string
+  hasNextPage: boolean
+  hasPreviousPage: boolean
+  startCursor: string
+  total: number
 }
 
-export default Index
-
-export const getStaticProps = async () => {
-  const response = await fetch(
-    process.env.GRAPHQL_ENDPOINT!,
-    {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ query }),
-    },
-  )
-  const data = await response.json()
-
-  return {
-    props:{
-      posts: data.data.posts.nodes
-    }
-  }
-}
-
-const query = `query getPosts {
-  posts(after: "1") {
+const postsQuery = `query getPosts {
+  posts(first: 10) {
     nodes {
       id
       slug
@@ -81,5 +55,130 @@ const query = `query getPosts {
         }
       }
     }
+    pageInfo {
+      total
+      endCursor
+      hasNextPage
+      hasPreviousPage
+      startCursor
+    }
   }
 }`;
+
+const nextPostsQuery = `query GetNextPostsQuery(
+  $endCursor: String!
+)
+{
+  posts(after: $endCursor) {
+    nodes {
+      id
+      slug
+      date
+      title
+      featuredImage {
+        node {
+          mediaItemUrl
+        }
+      }
+      excerpt
+      categories {
+        nodes {
+          name
+        }
+      }
+    }
+    pageInfo {
+      total
+      endCursor
+      hasNextPage
+      hasPreviousPage
+      startCursor
+    }
+  }
+}`;
+
+export const getStaticProps = async () => {
+  const response = await fetch(
+    process.env.GRAPHQL_ENDPOINT!,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ query: postsQuery }),
+    },
+  )
+  const data = await response.json()
+  const posts = data.data.posts.nodes.map((post: Post) => {
+    post.date = getDateDiff(post.date)
+    return post
+  })
+
+  return {
+    props:{
+      posts,
+      pageInfo: data.data.posts.pageInfo
+    }
+  }
+}
+
+const Index = ({ posts, pageInfo }: Props) => {
+  const [postsState, setPostsState] = useState(posts)
+  const [endCursor, setEndCursor] = useState(pageInfo.endCursor)
+  const [isNextPage, setIsNextPage] = useState(pageInfo.hasNextPage)
+
+  const getNextPosts = async() => {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!,
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          query: nextPostsQuery,
+          variables: {endCursor: endCursor},
+        }),
+      },
+    )
+    const data = await response.json()
+    const posts = data.data.posts.nodes.map((post: Post) => {
+      post.date = getDateDiff(post.date)
+      return post
+    })
+    setPostsState([...postsState, ...posts])
+    setEndCursor(data.data.posts.pageInfo.endCursor)
+    if(posts.length < 10) setIsNextPage(false)
+  }
+
+  const handleClick = () => {
+    getNextPosts()
+  }
+
+  return (
+    <>
+      <Container maxW="6xl">
+        <Box 
+          as="section"
+          py="10"
+          >
+          <Text fontSize="3xl" fontWeight="bold" textAlign="center">Posts</Text>
+          <Box mt="10">
+            <CardList posts={postsState}/>
+            {isNextPage && (
+              <Box mt="10" textAlign="center">
+                <Button 
+                  onClick={handleClick}
+                  colorScheme='blue' 
+                  variant='link'
+                  >
+                  もっと見る
+                </Button>
+              </Box>
+            )}
+          </Box>
+        </Box>
+      </Container>
+    </>
+  )
+}
+
+export default Index
