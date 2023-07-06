163484fdec9644dd570442c366e90a342f33b443
sun-njp <miuraasahi1@gmail.com>
2023-07-06 20:34:01 +0900

    GraphQL Code Generatorの設定変更

--- codegen.ts
+++ codegen.ts
@@ -3,16 +3,17 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: process.env.GRAPHQL_ENDPOINT,
-  documents: ['gql/query/*.graphql'],
-  ignoreNoDocuments: true,
+  // documents: ['gql/query/*.graphql'],
+  documents: ['components/**/*.{ts,tsx}', 'pages/**/*.{ts,tsx}'],
+  // ignoreNoDocuments: true,
   generates: {
-    "gql/generate/": {
+    "gql/": {
       preset: "client",
       plugins: []
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
+    // "./graphql.schema.json": {
+    //   plugins: ["introspection"]
+    // }
   }
 };
 
