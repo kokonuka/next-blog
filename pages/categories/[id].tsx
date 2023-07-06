d86911a0b719442860dfe004c61bcac172f6b06c
sun-njp <miuraasahi1@gmail.com>
2023-07-06 20:42:34 +0900

    GraphQL Code Generatorの適用

--- pages/categories/[id].tsx
+++ pages/categories/[id].tsx
@@ -1,71 +1,24 @@
-import { useContext, useEffect } from 'react';
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import { CategoryContext } from '../_app';
-import { Category } from '../../graphql/types/categories';
-import { ViewPost } from '../../graphql/types/posts';
-import { fetchGraph, fetchGraphWithVariable } from '../../graphql/fetchGraphql';
-import { getCategory, getCategoriesQuery } from '../../graphql/queries/categories';
-import { sliceText } from '../../lib/sliceText';
-import { getDateDiff } from '../../lib/getDateDiff';
-import { LawPosts } from '../../components/molecules/LawPosts';
-import { Container, Text, Box } from "@chakra-ui/react"
-import { DefaultLayout } from '../../components/templates/DefaultLayout';
-import { Post } from '../../gql/generate/graphql';
+import { useContext, useEffect, useState } from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { CategoryContext } from "../_app";
+import { PostsLayout } from "../../components/templates/PostsLayout";
+import { CategoryPosts } from "../../components/organisms/category/CategoryPosts";
 
-type Props = {
-  category: Category
-  posts: Post[]
-}
-
-const CategoryPage: NextPage<Props> = ({ category, posts }) => {
+const CategoryPage: NextPage = () => {
   const { setCarrentCategoryId } = useContext(CategoryContext);
+  const router = useRouter();
+  const [id, setId] = useState("");
 
-  useEffect(() => setCarrentCategoryId(category.id), [category]);
+  useEffect(() => {
+    const id = router.query.id;
+    if (id) {
+      setId(id as string);
+      setCarrentCategoryId(id as string);
+    }
+  }, [router, setCarrentCategoryId]);
 
-  return (
-    <DefaultLayout>
-      <Text color="gray.700" fontSize="3xl" fontWeight="bold" textAlign="center">{category.name}</Text>
-      <Box mt={3} whiteSpace="pre-wrap">{category.description}</Box>
-      <Box mt="10">
-        <LawPosts posts={posts}/>
-      </Box>
-    </DefaultLayout>
-  );
+  return <PostsLayout>{id != "" && <CategoryPosts id={id} />}</PostsLayout>;
 };
 
 export default CategoryPage;
-
-
-export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
-  const id = params!.id;
-  const data = await fetchGraphWithVariable(getCategory, { id });
-
-  const posts = data.category.contentNodes.nodes.map((post: ViewPost) => {
-    post.clippedTitle = sliceText(post.title);
-    post.dateDiff = getDateDiff(post.date);
-    return post;
-  });
-
-  return {
-    props: {
-      category: data.category,
-      posts: posts,
-    },
-  }
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const data = await fetchGraph(getCategoriesQuery);
-  const paths = data.categories.nodes.map((category: Category) => {
-    return {
-      params: {
-        id: `${category.id}`
-      }
-    }
-  });
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
--- pages/tags/[id].tsx
+++ pages/tags/[id].tsx
@@ -1,79 +1,92 @@
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import { Tag as TypeTag } from '../../graphql/types';
-import { ViewPost } from '../../graphql/types/posts';
-import { fetchGraphWithVariable } from '../../graphql/fetchGraphql';
-import { getTagsQuery, getNextTagsQuery, getPostTag } from "../../graphql/queries/tags";
-import { sliceText } from '../../lib/sliceText';
-import { getDateDiff } from '../../lib/getDateDiff';
-import { LawPosts } from '../../components/molecules/LawPosts';
-import { Container, Text, Box } from "@chakra-ui/react"
-import { DefaultLayout } from '../../components/templates/DefaultLayout';
-import { Post } from '../../gql/generate/graphql';
+import { NextPage } from "next";
+import {
+  getTagsQuery,
+  getNextTagsQuery,
+  getPostTag,
+} from "../../graphql/queries/tags";
+import { PostsLayout } from "../../components/templates/PostsLayout";
+import { TagPosts } from "../../components/organisms/tag/TagPosts";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-type Props = {
-  tag: TypeTag
-  posts: Post[]
-}
+const Tag: NextPage = () => {
+  const router = useRouter();
+  const [id, setId] = useState("");
 
-const Tag: NextPage<Props> = ({ tag, posts }) => {
+  useEffect(() => {
+    const id = router.query.id;
+    if (id) {
+      setId(id as string);
+    }
+  }, [router]);
+
   return (
-    <DefaultLayout>
-      <Text color="gray.700" fontSize="3xl" fontWeight="bold" textAlign="center">{tag.name}</Text>
+    <PostsLayout>
+      {/* <Text
+        color="gray.700"
+        fontSize="3xl"
+        fontWeight="bold"
+        textAlign="center"
+      >
+        {tag.name}
+      </Text>
       <Box mt="10">
-        <LawPosts posts={posts}/>
-      </Box>
-    </DefaultLayout>
+        <LawPosts posts={posts} />
+      </Box> */}
+      {id != "" && <TagPosts id={id} />}
+    </PostsLayout>
   );
 };
 
 export default Tag;
 
+// export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+//   const id = params!.id;
+//   const data = await fetchGraphWithVariable(getPostTag, { id });
 
-export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+//   const posts = data.tag.contentNodes.nodes.map((post: ViewPost) => {
+//     post.clippedTitle = sliceText(post.title);
+//     post.dateDiff = getFormattedDateTimeDiff(post.date);
+//     return post;
+//   });
 
-  const id = params!.id;
-  const data = await fetchGraphWithVariable(getPostTag, { id });
+//   return {
+//     props: {
+//       tag: data.tag,
+//       posts: data.tag.contentNodes.nodes,
+//     },
+//   };
+// };
 
-  const posts = data.tag.contentNodes.nodes.map((post: ViewPost) => {
-    post.clippedTitle = sliceText(post.title);
-    post.dateDiff = getDateDiff(post.date);
-    return post;
-  });
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   let tags: TypeTag[] = [];
 
-  return {
-    props: {
-      tag: data.tag,
-      posts: data.tag.contentNodes.nodes,
-    },
-  }
-};
+//   let hasNextPage = true;
+//   let endCursor = "";
+//   while (hasNextPage) {
+//     const data =
+//       tags.length == 0
+//         ? await fetchGraphWithVariable(getTagsQuery, { count: 10 })
+//         : await fetchGraphWithVariable(getNextTagsQuery, {
+//             endCursor: endCursor,
+//           });
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  let tags: TypeTag[] = [];
+//     tags.push(...data.tags.nodes);
+//     endCursor = data.tags.pageInfo.endCursor;
 
-  let hasNextPage = true;
-  let endCursor = "";
-  while (hasNextPage) {
-    const data = tags.length == 0 
-      ? await fetchGraphWithVariable(getTagsQuery, { "count": 10 }) 
-      : await fetchGraphWithVariable(getNextTagsQuery, { "endCursor": endCursor });
+//     if (!data.tags.pageInfo.hasNextPage) hasNextPage = false;
+//   }
 
-    tags.push(...data.tags.nodes);
-    endCursor = data.tags.pageInfo.endCursor;
+//   const paths = tags.map((tag: TypeTag) => {
+//     return {
+//       params: {
+//         id: `${tag.id}`,
+//       },
+//     };
+//   });
 
-    if(!data.tags.pageInfo.hasNextPage) hasNextPage = false;
-  };
-
-  const paths = tags.map((tag: TypeTag) => {
-    return {
-      params: {
-        id: `${tag.id}`
-      }
-    }
-  });
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
