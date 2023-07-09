4821c7d12090cf27ab648e6b1e8ef77096b9128c
sun-njp <miuraasahi1@gmail.com>
2023-07-10 00:01:24 +0900

    デザイン修正

--- components/organisms/AboutUs.tsx
+++ components/organisms/AboutUs.tsx
@@ -1,16 +1,28 @@
-import { Box, Text, Image, Link } from "@chakra-ui/react"
+import { Box, Text, Image, Link } from "@chakra-ui/react";
 import { AiOutlineTwitter } from "react-icons/ai";
 
 export const AboutUs = () => {
   return (
-    <Box as='section' >
-      <Text pb="5" color="gray.700" fontSize="3xl" fontWeight="bold">About</Text>
+    <Box
+      as="section"
+      mt="20"
+      py="10"
+      px="5"
+      bg="white"
+      borderRadius="3xl"
+      shadow="lg"
+    >
+      <Text color="gray.700" fontSize="3xl" fontWeight="bold">
+        About
+      </Text>
       <Image
-        objectFit='cover'
+        mt="3"
+        objectFit="cover"
         w="100%"
         h="30vh"
         src="images/image_01.jpg"
         alt="about"
+        borderRadius="lg"
       />
       <Box mt="3" display="flex" alignItems="center">
         <Text color="blue.400" fontSize="lg">
@@ -18,17 +30,20 @@ export const AboutUs = () => {
         </Text>
         <Link
           href={`https://twitter.com/${process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}`}
-          target="_blank" 
-          pl="1" 
+          target="_blank"
+          pl="1"
           fontSize="sm"
-          >@{process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}
+        >
+          @{process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}
         </Link>
       </Box>
       <Text mt="3" fontSize="xm">
-        こんにちは、ご訪問ありがとうございます。<br />
-        都内でWebエンジニアをしています。<br />
+        こんにちは、ご訪問ありがとうございます。
+        <br />
+        都内でWebエンジニアをしています。
+        <br />
         当ブログは開発で得たTipsのアウトプット兼、個人用のメモとなっています。
       </Text>
     </Box>
-  )
-}
+  );
+};
--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,4 +1,4 @@
-import { Container, Box, Text, Link } from "@chakra-ui/react"
+import { Container, Box, Text, Link } from "@chakra-ui/react";
 import { AiOutlineTwitter } from "react-icons/ai";
 
 export const Footer = () => {
@@ -6,14 +6,18 @@ export const Footer = () => {
     <Box as="footer">
       <Container maxW="6xl" p="5">
         <Box display="flex" justifyContent="center">
-          <Link href="https://twitter.com/sunrise_web_dev" target="_blank" fontSize="2xl">
-            <AiOutlineTwitter/>
+          <Link
+            href="https://twitter.com/sunrise_web_dev"
+            target="_blank"
+            fontSize="2xl"
+          >
+            <AiOutlineTwitter />
           </Link>
         </Box>
         <Text mt="3" fontSize="xs" textAlign="center">
-          © Copyright 2023 kimagurecode. All rights reserved.
+          © Copyright 2023 sun develop. All rights reserved.
         </Text>
       </Container>
     </Box>
-  )
-}
+  );
+};
