e085224feb5e45922ca6548e0ae0642e0045ba4a
sun-njp <miuraasahi1@gmail.com>
2023-07-09 23:55:35 +0900

    Prettierの適用

--- components/molecules/ColPosts.tsx
+++ components/molecules/ColPosts.tsx
@@ -1,21 +1,21 @@
-import { Box } from '@chakra-ui/react'
-import { Card } from './Card'
-import { Post } from '../../gql/generate/graphql'
+import { Box } from "@chakra-ui/react";
+import { Card } from "./Card";
+import { Post } from "../../graphql/generate/graphql";
 
 interface Props {
-  posts: Post[]
+  posts: Post[];
 }
-
 
 export const ColPosts: React.FC<Props> = ({ posts }) => {
   return (
-    <Box 
+    <Box
       display="grid"
-      gridTemplateColumns={{ base: "1fr 1fr", md: "1fr 1fr 1fr"}} 
-      gridGap="5" >
-      {posts.map((post) => (
-        <Card post={post} key={post.databaseId}/>
-      ))}
+      gridTemplateColumns={{ base: "1fr 1fr", md: "1fr 1fr 1fr" }}
+      gridGap="5"
+    >
+      {/* {posts.map((post) => (
+        <Card post={post} key={post.databaseId} />
+      ))} */}
     </Box>
-  )
-}
+  );
+};
