1068f0be87770751c82b44beca2fb19e227c175f
sun-njp <miuraasahi1@gmail.com>
2023-07-10 00:08:35 +0900

    Keyを追加

--- components/organisms/SideMenu.tsx
+++ components/organisms/SideMenu.tsx
@@ -1,20 +1,23 @@
-import { Link as Scroll } from 'react-scroll';
-import { IdsWithHeadings } from '../../pages/posts/[id]';
-import { Box } from '@chakra-ui/react';
+import { Link as Scroll } from "react-scroll";
+import { IdsWithHeadings } from "../../pages/posts/[id]";
+import { Box } from "@chakra-ui/react";
 
 type Props = {
-  idsWithHeadings: IdsWithHeadings[]
-}
+  idsWithHeadings: IdsWithHeadings[];
+};
 
 export const SideMenu: React.FC<Props> = ({ idsWithHeadings }) => {
   return (
     <Box bg="white" p="5" borderRadius="10">
       <p>目次</p>
-      {idsWithHeadings.map(id => (
-        <>
-          <Scroll to={`${id.id}`} smooth={true} >{id.heading}</Scroll><br />
-        </>
+      {idsWithHeadings.map((id) => (
+        <Box key={id.id}>
+          <Scroll to={`${id.id}`} smooth={true}>
+            {id.heading}
+          </Scroll>
+          <br />
+        </Box>
       ))}
     </Box>
-  )
-}
+  );
+};
