2951380477baa2395cfd7f7c41ebda06359c4996
sun-njp <miuraasahi1@gmail.com>
2023-07-09 17:20:19 +0900

    レイアウトを修正

--- components/templates/PostLayout.tsx
+++ components/templates/PostLayout.tsx
@@ -1,26 +1,29 @@
-import { Header } from "../organisms/Header"
-import { Footer } from "../organisms/Footer"
-import { Box, Container, Text } from "@chakra-ui/react"
-import { Post, Tag } from "../../gql/generate/graphql"
-import { IdsWithHeadings } from "../../pages/posts/[id]"
-import styles from '../../styles/Post.module.css';
-import { SideMenu } from "../organisms/SideMenu"
-import { Tags } from "../organisms/Tags"
+import { Header } from "../organisms/Header";
+import { Footer } from "../organisms/Footer";
+import { Box, Container, Text } from "@chakra-ui/react";
+import { Post, Tag } from "../../graphql/generate/graphql";
+import { IdsWithHeadings } from "../../pages/posts/[id]";
+import styles from "../../styles/Post.module.css";
+import { SideMenu } from "../organisms/SideMenu";
+import { Tags } from "../organisms/Tags";
 
 type Props = {
-  post: Post
-  content: string
-  idsWithHeadings: IdsWithHeadings[]
-}
+  post: Post;
+  content: string;
+  idsWithHeadings: IdsWithHeadings[];
+};
 
-
-export const PostLayout: React.FC<Props> = ({ post, content, idsWithHeadings }) => {
-  const tags = post?.tags?.nodes as Tag[]
+export const PostLayout: React.FC<Props> = ({
+  post,
+  content,
+  idsWithHeadings,
+}) => {
+  const tags = post?.tags?.nodes as Tag[];
 
   return (
     <>
       <Header />
-      <Box bg="gray.50" >
+      <Box bg="gray.50">
         <Container
           as="main"
           maxW="6xl"
@@ -29,21 +32,36 @@ export const PostLayout: React.FC<Props> = ({ post, content, idsWithHeadings }) 
           px={{ base: "0", lg: "4" }}
           display="flex"
           flexDirection="column"
-          flex="1" >
+          flex="1"
+        >
           <Box p="5">
-            <Text fontSize={{base: "2xl", lg: "4xl" }} color="gray.700">
+            <Text fontSize={{ base: "2xl", lg: "4xl" }} color="gray.700">
               {post.title}
             </Text>
             <Text mt="5" color="gray.500">
-              {post.date}{/* {post.formattedDate} */}
+              {post.date}
+              {/* {post.formattedDate} */}
             </Text>
           </Box>
           <Box mt="10" display={{ base: "block", lg: "flex" }}>
-            <Box width={{ base: "100%", lg: "60%" }} bg="white" p="5" borderRadius={{ base: "0", lg: "10" }} py="10">
+            <Box
+              width={{ base: "100%", lg: "60%" }}
+              bg="white"
+              p="5"
+              borderRadius={{ base: "0", lg: "10" }}
+              py="10"
+            >
               <Tags tags={tags} />
-              <div className={`post znc ${styles.body}`} dangerouslySetInnerHTML={{ __html: content }} ></div>
+              <div
+                className={`post znc ${styles.body}`}
+                dangerouslySetInnerHTML={{ __html: content }}
+              ></div>
             </Box>
-            <Box display={{ base: "none", lg: "block" }} width={{ base: "100%", lg: "40%" }} pl="5">
+            <Box
+              display={{ base: "none", lg: "block" }}
+              width={{ base: "100%", lg: "40%" }}
+              pl="5"
+            >
               <SideMenu idsWithHeadings={idsWithHeadings} />
             </Box>
           </Box>
@@ -51,5 +69,5 @@ export const PostLayout: React.FC<Props> = ({ post, content, idsWithHeadings }) 
       </Box>
       <Footer />
     </>
-  )
-}
+  );
+};
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,39 +1,45 @@
 import { Head } from "../../components/Head";
-import { GetStaticProps, GetStaticPaths, NextPage } from "next"
-import { load } from 'cheerio';
-import hljs from 'highlight.js';
-import 'highlight.js/styles/base16/nova.css';
+import { GetStaticProps, GetStaticPaths, NextPage } from "next";
+import { load } from "cheerio";
+import hljs from "highlight.js";
+import "highlight.js/styles/base16/nova.css";
 import { fetchGraphWithVariable } from "../../graphql/fetchGraphql";
 import { getPostsQuery, getNextPostsQuery } from "../../graphql/queries/posts";
-import { GetPostDocument } from "../../gql/generate/graphql";
+import { GetPostDocument } from "../../graphql/generate/graphql";
 import { PostLayout } from "../../components/templates/PostLayout";
-import 'zenn-content-css';
-import { Post } from "../../gql/generate/graphql";
+import "zenn-content-css";
+// import { Post } from "../../graphql/types/posts";
+import { Post, Tag } from "../../graphql/generate/graphql";
 import client from "../../lib/graphqlClient";
 
 export type IdsWithHeadings = {
-  id: string,
-  heading: string
-}
+  id: string;
+  heading: string;
+};
 
 export type Props = {
-  post: Post
-  content: string
-  idsWithHeadings: IdsWithHeadings[]
-}
-
+  post: Post;
+  content: string;
+  idsWithHeadings: IdsWithHeadings[];
+};
 
 const PostPage: NextPage<Props> = ({ post, content, idsWithHeadings }) => {
   return (
     <>
-      <Head title={`${post.title} | sun develop`} description='Webエンジニアの備忘録' />
-      <PostLayout post={post} content={content} idsWithHeadings={idsWithHeadings} />
+      <Head
+        title={`${post.title} | sun develop`}
+        description="Webエンジニアの備忘録"
+      />
+      <PostLayout
+        post={post}
+        content={content}
+        idsWithHeadings={idsWithHeadings}
+      />
     </>
-  )
-}
+  );
+};
 
 export default PostPage;
-
 
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   const id = params?.id as string;
@@ -41,67 +47,69 @@ export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   const { data } = await client.query({
     query: GetPostDocument,
     variables: {
-      id
-    }
-  }); 
-  const post = data?.post as Post
+      id,
+    },
+  });
+  const post = data?.post as Post;
 
   const $ = load(post.content!);
 
   // codeタグにhighlight.jsを適用
-  const codes = $('pre code');
+  const codes = $("pre code");
   codes.each((_, elm) => {
-    const result = hljs.highlightAuto( $(elm).text() );
+    const result = hljs.highlightAuto($(elm).text());
     $(elm).html(result.value);
-    $(elm).addClass('hljs');
+    $(elm).addClass("hljs");
   });
 
   // 見出しにIDを追加
-  const headings = $('h1, h2, h3, h4, h5, h6');
+  const headings = $("h1, h2, h3, h4, h5, h6");
   let idsWithHeadings: IdsWithHeadings[] = [];
   headings.each((index, elm) => {
     const heading = $(elm).text();
     const id = `${elm.tagName}-${index}`;
-    $(elm).attr('id', id);
+    $(elm).attr("id", id);
 
     idsWithHeadings.push({
       id,
-      heading
+      heading,
     });
-  })
+  });
 
   return {
     props: {
       post,
       content: $.html(),
       idsWithHeadings,
-    }
-  }
+    },
+  };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-
   let posts: Post[] = [];
 
   let hasNextPage = true;
   let endCursor = "";
   while (hasNextPage) {
-    const data = posts.length == 0 
-      ? await fetchGraphWithVariable(getPostsQuery, { "count": 10 }) 
-      : await fetchGraphWithVariable(getNextPostsQuery, { "endCursor": endCursor });
+    const data =
+      posts.length == 0
+        ? await fetchGraphWithVariable(getPostsQuery, { count: 10 })
+        : await fetchGraphWithVariable(getNextPostsQuery, {
+            endCursor: endCursor,
+          });
 
     posts.push(...data.posts.nodes);
     endCursor = data.posts.pageInfo.endCursor;
 
-    if(!data.posts.pageInfo.hasNextPage) hasNextPage = false;
-  };
+    if (!data.posts.pageInfo.hasNextPage) hasNextPage = false;
+  }
 
   const paths = posts.map((post: Post) => {
     return {
       params: {
-        id: `${post.databaseId}`
-      }
-    }
+        id: `${post.databaseId}`,
+      },
+    };
   });
 
   return {
