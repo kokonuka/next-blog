07700693fd345f3580cecf015aed1fa5f40206d9
sun-njp <miuraasahi1@gmail.com>
2023-07-10 00:03:39 +0900

    サイドメニューの削除

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,36 +1,43 @@
-import { useRef } from "react";
-import Link from "next/link"
-import {
-  useDisclosure,
-  Container,
-  Box,
-} from '@chakra-ui/react'
-import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
-import { AiFillHome } from "react-icons/ai";
-import Drawer from "../drawer";
-import Categories from "../categories";
+import NextLink from "next/link";
+import { Container, Box, Link } from "@chakra-ui/react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaRegFileAlt } from "react-icons/fa";
 
 export const Header = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = useRef(null)
-
   return (
     <>
       <Box bg="white" py="2" as="header">
-        <Container maxW='6xl' display="flex" justifyContent="space-between">
-          <Box display="flex" alignItems="center" fontSize="2xl" color="gray.600">
-            <Link href="/search"><AiOutlineSearch /></Link>
+        <Container maxW="6xl" display="flex" justifyContent="space-between">
+          <Box
+            display="flex"
+            alignItems="center"
+            fontSize="2xl"
+            color="gray.800"
+          >
+            <Link
+              as={NextLink}
+              href="/"
+              display="flex"
+              alignItems="center"
+              gap="2"
+              _hover={{}}
+            >
+              <FaRegFileAlt />
+              {process.env.SITE_TITLE || process.env.NEXT_PUBLIC_SITE_TITLE}
+            </Link>
           </Box>
-          <Box display="flex" alignItems="center" fontSize="2xl" color="gray.800">
-            <Link href="/"><AiFillHome /></Link>
-          </Box>
-          <Box ref={btnRef} onClick={onOpen} display="flex" alignItems="center" fontSize="2xl" color="gray.600" cursor="pointer">
-            <AiOutlineMenu />
+          <Box
+            display="flex"
+            alignItems="center"
+            fontSize="2xl"
+            color="gray.600"
+          >
+            <Link href="/search">
+              <AiOutlineSearch />
+            </Link>
           </Box>
         </Container>
       </Box>
-      <Drawer isOpen={isOpen} onOpen={onOpen} onClose={onClose} btnRef={btnRef} />
-      <Categories />
     </>
-  )
-}
+  );
+};
