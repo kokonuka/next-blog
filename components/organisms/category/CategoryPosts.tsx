e15167e9a30f7938d340f11dceaf46ef2add2530
sun-njp <miuraasahi1@gmail.com>
2023-07-10 00:08:17 +0900

    Reduxの導入

--- components/organisms/category/CategoryPosts.tsx
+++ components/organisms/category/CategoryPosts.tsx
@@ -3,10 +3,9 @@ import { Box } from "@chakra-ui/react";
 import { HeadingWithDescription } from "../../molecules/HeadingWithDescription";
 import { CategoryPostList } from "./CategoryPostList";
 import { useQuery } from "@apollo/client";
+import { useAppSelector } from "../../../redux/store";
 
-type Props = {
-  id: string;
-};
+type Props = {};
 
 export const categoryWithVariablesQueryDocument = graphql(`
   query categoryWithVariablesQuery($id: ID!) {
@@ -18,7 +17,9 @@ export const categoryWithVariablesQueryDocument = graphql(`
   }
 `);
 
-export const CategoryPosts: React.FC<Props> = ({ id }) => {
+export const CategoryPosts: React.FC<Props> = () => {
+  const id = useAppSelector((state) => state.categoryId.id);
+
   const { loading, error, data } = useQuery(
     categoryWithVariablesQueryDocument,
     { variables: { id } }
@@ -32,7 +33,7 @@ export const CategoryPosts: React.FC<Props> = ({ id }) => {
         text={data?.category?.name!}
         description={data?.category?.description!}
       />
-      <CategoryPostList id={id} />
+      <CategoryPostList />
     </Box>
   );
 };
