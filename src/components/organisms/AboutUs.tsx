5a8e26c00c22fe4850deca7a0786b40accc8988d
sun-njp <miuraasahi1@gmail.com>
2023-07-10 11:16:01 +0900

    デザインの変更

--- src/components/organisms/AboutUs.tsx
+++ src/components/organisms/AboutUs.tsx
@@ -1,40 +1,42 @@
-import { Box, Text, Image, Link } from "@chakra-ui/react";
+import { Box, Text, Image, Link, Heading } from "@chakra-ui/react";
 import { AiOutlineTwitter } from "react-icons/ai";
 
 export const AboutUs = () => {
   return (
-    <Box as="section" mt="20" p={{ base: "5", lg: "10" }} bg="white">
-      <Text color="gray.700" fontSize="3xl" fontWeight="bold">
+    <Box as="section" mt="20">
+      <Heading color="gray.700" fontSize="5xl" fontWeight="bold">
         About
-      </Text>
-      <Image
-        mt="3"
-        objectFit="cover"
-        w="100%"
-        h="30vh"
-        src="images/image_01.jpg"
-        alt="about"
-      />
-      <Box mt="3" display="flex" alignItems="center">
-        <Text color="blue.400" fontSize="lg">
-          <AiOutlineTwitter />
+      </Heading>
+      <Box mt="5" bg="white" p="5">
+        <Image
+          mt="3"
+          objectFit="cover"
+          w="100%"
+          h="30vh"
+          src="images/image_01.jpg"
+          alt="about"
+        />
+        <Box mt="5" display="flex" alignItems="center">
+          <Text color="blue.400" fontSize="lg">
+            <AiOutlineTwitter />
+          </Text>
+          <Link
+            href={`https://twitter.com/${process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}`}
+            target="_blank"
+            pl="1"
+            fontSize="sm"
+          >
+            @{process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}
+          </Link>
+        </Box>
+        <Text mt="5" pb="10" fontSize="xm">
+          こんにちは、ご訪問ありがとうございます。
+          <br />
+          都内でWebエンジニアをしています。
+          <br />
+          当ブログは開発で得たTipsのアウトプット兼、個人用のメモとなっています。
         </Text>
-        <Link
-          href={`https://twitter.com/${process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}`}
-          target="_blank"
-          pl="1"
-          fontSize="sm"
-        >
-          @{process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}
-        </Link>
       </Box>
-      <Text mt="3" fontSize="xm">
-        こんにちは、ご訪問ありがとうございます。
-        <br />
-        都内でWebエンジニアをしています。
-        <br />
-        当ブログは開発で得たTipsのアウトプット兼、個人用のメモとなっています。
-      </Text>
     </Box>
   );
 };
