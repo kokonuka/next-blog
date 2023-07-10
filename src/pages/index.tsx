a1ca965bd89517f6d2490d5faef1c46d3ff21ada
sun-njp <miuraasahi1@gmail.com>
2023-07-10 11:04:08 +0900

    ローディングコンテキストを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,18 @@ import type { NextPage } from "next";
 import { HomeLayout } from "../components/templates/HomeLayout";
 import { AboutUs } from "../components/organisms/AboutUs";
 import { CategoryPostsSlider } from "../components/organisms/CategoryPostsSlider";
-import { PageLoading } from "../components/organisms/PageLoading";
-import { LoadingContextProvider } from "../context/LoadingContext";
 
 type Props = {};
 
 const Home: NextPage<Props> = () => {
-  console.log("index hoge");
   return (
     <>
       <Head title="sun develop" description="Webエンジニアの備忘録" />
-      <LoadingContextProvider>
-        <HomeLayout>
-          <CategoryPostsSlider name="Tech" />
-          <CategoryPostsSlider name="CheatSheet" />
-          <AboutUs />
-          <PageLoading />
-        </HomeLayout>
-      </LoadingContextProvider>
+      <HomeLayout>
+        <CategoryPostsSlider name="Tech" />
+        <CategoryPostsSlider name="CheatSheet" />
+        <AboutUs />
+      </HomeLayout>
     </>
   );
 };
