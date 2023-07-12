fdb199bf8c2597c6bd59f5b1e2c5ef04540edd51
sun-njp <miuraasahi1@gmail.com>
2023-07-12 21:30:30 +0900

    レイアウトを変更

--- src/components/organisms/AboutUs.tsx
+++ src/components/organisms/AboutUs.tsx
@@ -7,35 +7,38 @@ export const AboutUs = () => {
       <Heading color="gray.700" fontSize="5xl" fontWeight="bold">
         About
       </Heading>
-      <Box mt="5" bg="white" p="5">
-        <Image
-          mt="3"
-          objectFit="cover"
-          w="100%"
-          h="30vh"
-          src="images/image_01.jpg"
-          alt="about"
-        />
-        <Box mt="5" display="flex" alignItems="center">
-          <Text color="blue.400" fontSize="lg">
-            <AiOutlineTwitter />
-          </Text>
-          <Link
-            href={`https://twitter.com/${process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}`}
-            target="_blank"
-            pl="1"
-            fontSize="sm"
-          >
-            @{process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}
-          </Link>
+      <Box mt="5" bg="white" p="5" display={{ base: "block", lg: "flex" }}>
+        <Box>
+          <Image
+            objectFit="cover"
+            w="100%"
+            h="30vh"
+            src="images/image_01.jpg"
+            alt="about"
+          />
         </Box>
-        <Text mt="5" pb="10" fontSize="xm">
-          こんにちは、ご訪問ありがとうございます。
-          <br />
-          都内でWebエンジニアをしています。
-          <br />
-          当ブログは開発で得たTipsのアウトプット兼、個人用のメモとなっています。
-        </Text>
+        <Box pl={{ base: "0", lg: "5" }}>
+          <Box mt="5" display="flex" alignItems="center">
+            <Text color="blue.400" fontSize="lg">
+              <AiOutlineTwitter />
+            </Text>
+            <Link
+              href={`https://twitter.com/${process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}`}
+              target="_blank"
+              pl="1"
+              fontSize="sm"
+            >
+              @{process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}
+            </Link>
+          </Box>
+          <Text mt="5" pb="10" fontSize="xm">
+            こんにちは、ご訪問ありがとうございます。
+            <br />
+            都内でWebエンジニアをしています。
+            <br />
+            当ブログは開発で得たTipsのアウトプット兼、個人用のメモとなっています。
+          </Text>
+        </Box>
       </Box>
     </Box>
   );
