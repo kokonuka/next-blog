400109f51d63433d9718318f1ab4b5634ff71c1d
sun-njp <miuraasahi1@gmail.com>
2023-07-14 01:53:19 +0900

    リファクタリング

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -6,8 +6,8 @@ import { getPostsOfSearch } from "../../graphql/queries/posts";
 import { fetchGraphWithVariable } from "../../graphql/fetchGraphql";
 import { Post } from "../../graphql/generate/graphql";
 import { SearchLayout } from "../../components/templates/SearchLayout";
-import { AllTags } from "../../components/organisms/AllTags";
-import { SearchInput } from "../../components/molecules/SearchInput";
+import { Tags } from "../../components/organisms/search/Tags";
+import { SearchInput } from "../../components/organisms/search/SearchInput";
 
 type Props = {};
 
@@ -19,7 +19,6 @@ const fetchPostsOfQuery = async (q: string | string[]) => {
 const SearchPage: NextPage<Props> = () => {
   const router = useRouter();
   const { q } = router.query;
-  const [value, setValue] = useState("");
   const [posts, setPosts] = useState<Post[]>([]);
   const [isPostsLoading, setIsPostsLoading] = useState(true);
 
@@ -30,7 +29,6 @@ const SearchPage: NextPage<Props> = () => {
       return;
     }
 
-    setValue(q as string);
     (async () => {
       const post = await fetchPostsOfQuery(q);
       setPosts(post);
@@ -46,12 +44,8 @@ const SearchPage: NextPage<Props> = () => {
     <>
       <Head title="Search | sun develop" description="Webエンジニアの備忘録" />
       <SearchLayout>
-        <SearchInput
-          value={value}
-          setValue={setValue}
-          setIsPostsLoading={setIsPostsLoading}
-        />
-        <AllTags />
+        <SearchInput setIsPostsLoading={setIsPostsLoading} />
+        <Tags />
         {/* <ResultPosts posts={posts} isPostsLoading={isPostsLoading} /> */}
       </SearchLayout>
     </>
