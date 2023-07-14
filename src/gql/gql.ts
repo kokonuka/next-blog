e897714e76b1ab42f364af53055d861cbef09f73
sun-njp <miuraasahi1@gmail.com>
2023-07-15 00:23:59 +0900

    GraphQL Code Generatorの型を生成

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -13,12 +13,15 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "\n  fragment TagButton on Tag {\n    id\n    name\n  }\n": types.TagButtonFragmentDoc,
     "\n  fragment TagItem on Tag {\n    id\n    name\n  }\n": types.TagItemFragmentDoc,
     "\n  fragment PostItem on Post {\n    id\n    databaseId\n    title\n    date\n    content\n    featuredImage {\n      node {\n        mediaItemUrl\n      }\n    }\n    categories {\n      nodes {\n        id\n        name\n      }\n    }\n    tags {\n      nodes {\n        ...TagItem\n      }\n    }\n  }\n": types.PostItemFragmentDoc,
     "\n  query categoryWhereCategoryNameQuery($name: [String]) {\n    categories(where: { name: $name }) {\n      nodes {\n        id\n        name\n        posts {\n          nodes {\n            ...PostItem\n          }\n        }\n      }\n    }\n  }\n": types.CategoryWhereCategoryNameQueryDocument,
     "\n  query allPostsWithVariablesQuery($endCursor: String!) {\n    posts(first: 10, after: $endCursor) {\n      nodes {\n        ...PostItem\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n": types.AllPostsWithVariablesQueryDocument,
     "\n  query allPostsWithCategoryQuery($categoryIn: [ID], $endCursor: String!) {\n    posts(where: { categoryIn: $categoryIn }, first: 10, after: $endCursor) {\n      nodes {\n        ...PostItem\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n": types.AllPostsWithCategoryQueryDocument,
     "\n  query categoryWithVariablesQuery($id: ID!) {\n    category(id: $id, idType: ID) {\n      id\n      name\n      description\n    }\n  }\n": types.CategoryWithVariablesQueryDocument,
+    "\n  query postsWhereSearchQuery($keyword: String!, $endCursor: String!) {\n    posts(where: { search: $keyword }, first: 10, after: $endCursor) {\n      nodes {\n        ...PostItem\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n": types.PostsWhereSearchQueryDocument,
+    "\n  query allTagsQuery($endCursor: String!) {\n    tags(first: 10, after: $endCursor) {\n      nodes {\n        ...TagButton\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n": types.AllTagsQueryDocument,
     "\n  query allPostsByTagIdWithVariablesQuery($tagIn: [ID], $endCursor: String!) {\n    posts(where: { tagIn: $tagIn }, first: 10, after: $endCursor) {\n      nodes {\n        ...PostItem\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n": types.AllPostsByTagIdWithVariablesQueryDocument,
     "\n  query tagWithVariablesQuery($id: ID!) {\n    tag(id: $id, idType: ID) {\n      id\n      name\n      description\n    }\n  }\n": types.TagWithVariablesQueryDocument,
 };
@@ -37,6 +40,10 @@ const documents = {
  */
 export function graphql(source: string): unknown;
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  fragment TagButton on Tag {\n    id\n    name\n  }\n"): (typeof documents)["\n  fragment TagButton on Tag {\n    id\n    name\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
@@ -61,6 +68,14 @@ export function graphql(source: "\n  query allPostsWithCategoryQuery($categoryIn
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n  query categoryWithVariablesQuery($id: ID!) {\n    category(id: $id, idType: ID) {\n      id\n      name\n      description\n    }\n  }\n"): (typeof documents)["\n  query categoryWithVariablesQuery($id: ID!) {\n    category(id: $id, idType: ID) {\n      id\n      name\n      description\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  query postsWhereSearchQuery($keyword: String!, $endCursor: String!) {\n    posts(where: { search: $keyword }, first: 10, after: $endCursor) {\n      nodes {\n        ...PostItem\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n"): (typeof documents)["\n  query postsWhereSearchQuery($keyword: String!, $endCursor: String!) {\n    posts(where: { search: $keyword }, first: 10, after: $endCursor) {\n      nodes {\n        ...PostItem\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  query allTagsQuery($endCursor: String!) {\n    tags(first: 10, after: $endCursor) {\n      nodes {\n        ...TagButton\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n"): (typeof documents)["\n  query allTagsQuery($endCursor: String!) {\n    tags(first: 10, after: $endCursor) {\n      nodes {\n        ...TagButton\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n      }\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
