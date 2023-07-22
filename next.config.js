b7d008a01c170e640c7db2614d2dc7564c769481
sun-njp <miuraasahi1@gmail.com>
2023-07-22 17:07:46 +0900

    画像のCORS対応

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.kimagurecode.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
