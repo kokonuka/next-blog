2e08dd4aae3ce5f6ef5f9c2c7fc788868676834c
sun-njp <miuraasahi1@gmail.com>
2023-08-11 20:11:05 +0900

    コンポーネント名を変更

---
+++ src/components/organisms/Profile.tsx
@@ -0,0 +1,62 @@
+import { Box, Text, Link, Avatar, useColorModeValue } from "@chakra-ui/react";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+
+export const Profile = () => {
+  return (
+    <Box
+      as="section"
+      py="10"
+      px="5"
+      bg={useColorModeValue("white", "gray.800")}
+      border={useColorModeValue("0", "1px")}
+      borderRadius={useColorModeValue("0", "lg")}
+      borderColor={useColorModeValue("gray.200", "gray.700")}
+    >
+      <Box display="flex" justifyContent="center">
+        <Avatar
+          size="2xl"
+          name="Dan Abrahmov"
+          src="images/image_01.jpg"
+          objectFit="contain"
+        />
+      </Box>
+      <Box
+        mt="5"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        flex="1"
+      >
+        <Text textAlign="center">
+          {process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}
+        </Text>
+        <Box mt="3" display="flex" justifyContent="center" gap="2">
+          <Link
+            href={`https://twitter.com/${process.env.NEXT_PUBLIC_TWITTER_ACCOUNT_NAME}`}
+            target="_blank"
+            fontSize="2xl"
+          >
+            <AiOutlineTwitter />
+          </Link>
+          <Link
+            href={`https://github.com/${process.env.NEXT_PUBLIC_GITHUB_ACCOUNT_NAME}`}
+            target="_blank"
+            fontSize="2xl"
+          >
+            <FaGithub />
+          </Link>
+        </Box>
+      </Box>
+      <Box>
+        <Text mt="7" fontSize="xm" textAlign="center">
+          ご訪問ありがとうございます。
+          <br />
+          都内でWebエンジニアをしています。
+          <br />
+          当ブログでは開発で得たTipsのアウトプットをしています。
+        </Text>
+      </Box>
+    </Box>
+  );
+};
