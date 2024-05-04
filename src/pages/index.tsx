9069ea3e626d07c68e8ddb43448c20b6290f7e61
kokonuka <miuraasahi1@gmail.com>
2024-05-05 08:33:02 +0900

    メモを非表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Head } from "../components/Head";
-import type { NextPage } from "next";
-import { HomeLayout } from "../components/templates/HomeLayout";
-import { Box } from "@chakra-ui/react";
-import TechPostList from "@/components/organisms/home/TechPostList";
-import CheatSheetPostList from "@/components/organisms/home/CheatSheetPostList";
+import React from 'react';
+import { Head } from '../components/Head';
+import type { NextPage } from 'next';
+import { HomeLayout } from '../components/templates/HomeLayout';
+import { Box } from '@chakra-ui/react';
+import TechPostList from '@/components/organisms/home/TechPostList';
+import CheatSheetPostList from '@/components/organisms/home/CheatSheetPostList';
 
 type Props = {};
 
@@ -15,7 +15,7 @@ const Home: NextPage<Props> = () => {
       <HomeLayout>
         <Box as="main">
           <TechPostList />
-          <CheatSheetPostList />
+          {/* <CheatSheetPostList /> */}
         </Box>
       </HomeLayout>
     </>
