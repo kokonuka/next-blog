cd609c559aaab2fd1f96ba4cb83ba28ca3b37578
kokonuka <miuraasahi1@gmail.com>
2024-05-05 08:32:15 +0900

    デフォルトテーマカラーを変更

--- src/components/thema.ts
+++ src/components/thema.ts
@@ -1,22 +1,22 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
-import { mode } from "@chakra-ui/theme-tools";
-import type { StyleFunctionProps } from "@chakra-ui/styled-system";
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+import type { StyleFunctionProps } from '@chakra-ui/styled-system';
 
 const colors = {
-  white: "#fff",
+  white: '#fff',
 };
 
 const config: ThemeConfig = {
-  initialColorMode: "light",
+  initialColorMode: 'dark',
   useSystemColorMode: false,
 };
 
 const styles = {
   global: (props: StyleFunctionProps) => ({
     body: {
-      fontFamily: "body",
-      color: mode("gray.800", "whiteAlpha.900")(props),
-      bg: mode("blackAlpha.50", "rgb(25, 25, 25)")(props),
+      fontFamily: 'body',
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('blackAlpha.50', 'rgb(25, 25, 25)')(props),
     },
   }),
 };
